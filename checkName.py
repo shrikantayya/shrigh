44926058cc8cbbbed38e3803ec3abcb22882517b
shrikantayya <43928172+shrikantayya@users.noreply.github.com>
2019-07-16 09:00:26 +0530

    Add files via upload
    test programs

---
+++ checkName.py
@@ -0,0 +1,17 @@
+def main():
+    a=eval(input("ennter the chances"))
+    count = a
+    while(count!=0):
+        count = count - 1
+        name = str(input("Enter the name"))
+        if(name == "python"):
+            break
+        else:
+            continue
+
+        break
+
+
+main()
+
+
